acf17ea8b0a3f498075fea616a2d9eaaf6c88e04
Sam Elkhateeb <same@nanosn.com>
2012-11-25 16:31:24 -0700

    implements laziness across all parser classess

--- lib/parser.dart
+++ lib/parser.dart
@@ -2,23 +2,66 @@ library parse;
 
 import 'dart:core';
 
+/** Utility function for sets equality */
+bool set_equals(Set left, Set right){
+  if(left == null || right == null)
+    return false;
+ 
+  //simplest case
+  if(left.length != right.length)
+    return false;
+
+  //clone one of the sets
+  var test = new Set.from(left);
+  for(var value in right){
+    if(test.contains(value))
+      test.remove(value);
+    else
+      return false;
+  }
+  return test.isEmpty;
+}
+
 abstract class Parser {
 
   And operator +(Parser right){
     return new And(this, right);
   }
+  
+  Or operator |(Parser right){
+    return new Or(this, right);
+  }
+  
   And operator *(Parser right){
     if( right != null)
       throw 'right of * must be null';
     return null;
   }
-  
+
+  static final _cache = {};
+  Set _set;
   bool isNullable();
   bool isEmpty();
-  Set  parseNull();
-  Parser derive(token);
+  Parser _derive(token);
+  Set  _parseNull();
+  Set  parseNull(){
+    if(this._set != null)
+      return this._set;
+
+    Set anotherSet = new Set();
+    do {
+      this._set = anotherSet;
+      anotherSet = this._parseNull();
+    } while(!set_equals(this._set, anotherSet));
+  }
   
-  List parse(List tokens){
+  Parser derive(token){
+    if(_cache[token] == null)
+      _cache[token] = new Lazy(this, token);
+    return _cache[token];
+  }
+  
+  Set parse(List tokens){
     var parser = this;
     for( var token in tokens){
       parser = parser.derive(token);
@@ -27,11 +70,38 @@ abstract class Parser {
   }
 }
 
+class Lazy extends Parser {
+  Parser parser;
+  var token;
+  Parser derivative;
+
+  Lazy(this.parser, this.token);
+  Parser _derive(token){
+    print('derive lazy $token');
+    this._evaluate();
+    return this.derivative.derive(token);
+  }
+
+  Set _parseNull(){
+    print('derive lazy $token');
+    this._evaluate();
+    return this.derivative.parseNull();
+  }
+  
+  void _evaluate(){
+    if(this.derivative == null)
+      this.derivative = this.parser._derive(this.token);
+  }
+}
+
 class Empty extends Parser {
   bool isNullable() => false;
   bool isEmpty() => true;
-  Set parseNull() => new Set();
-  Parser derive(token) => new Empty();
+  Set _parseNull() => new Set();
+  Parser _derive(token) {
+    print('derive empty');
+    return new Empty();
+  }
 
   /** singleton factory */
   static final _instance = new Empty._internal();
@@ -45,8 +115,8 @@ class Null extends Parser {
   
   bool isNullable() => true;
   bool isEmpty() => false;
-  Set parseNull() => this.result;
-  Parser derive(token) => new Empty();
+  Set _parseNull() => this.result;
+  Parser _derive(token) => new Empty();
 
   /** singleton factory */
   static Null _instance;
@@ -64,7 +134,7 @@ class Null extends Parser {
       this.result = result;
     } else {
       this.result = new Set();
-      this.resutt.add(null);
+      this.result.add(null);
     }
   }
 }
@@ -75,8 +145,8 @@ class Token extends Parser {
 
   bool isNullable() => false;
   bool isEmpty() => false;
-  Set parseNull() => new Set();
-  Parser derive(token){
+  Set _parseNull() => new Set();
+  Parser _derive(token){
     print('Token derive $token');
     if (this.token == token){
       var s = new Set();
@@ -93,8 +163,8 @@ class RegEx extends Parser {
 
   bool isNullable() => false;
   bool isEmpty() => false;
-  Set parseNull() => new Set();
-  Parser derive(token){
+  Set _parseNull() => new Set();
+  Parser _derive(token){
     RegExp exp = new RegExp(this.pattern);
     if (exp.hasMatch(token)){
       var s = new Set();
@@ -112,7 +182,7 @@ class And extends Parser {
 
   bool isNullable() => this.first.isNullable() && this.second.isNullable();
   bool isEmpty() => this.first.isNullable() || this.second.isNullable();
-  Set parseNull(){
+  Set _parseNull(){
     Set result = new Set();
     for ( var l in this.first.parseNull()) {
       for (var r in this.second.parseNull())
@@ -121,7 +191,7 @@ class And extends Parser {
     return result;
   }
   
-  Parser derive(token){
+  Parser _derive(token){
     print('And derive $token');
     if (this.first.isNullable()){
       var left = new And(this.first.derive(token), this.second);
@@ -143,14 +213,14 @@ class Or extends Parser {
   bool isNullable() => this.first.isNullable() || this.second.isNullable();
   bool isEmpty() => this.first.isNullable() && this.second.isNullable();
 
-  Set parseNull(){
+  Set _parseNull(){
     Set result = new Set();
     result.addAll(this.first.parseNull());
     result.addAll(this.second.parseNull());
     return result;
   }
   
-  Parser derive(token){
+  Parser _derive(token){
     if (this.first.isEmpty()){
       return this.second.derive(token);
     }
@@ -161,3 +231,9 @@ class Or extends Parser {
   }
 }
 
+class KleeneStar extends Parser {
+  Parser parser;
+  KleeneStar(this.parser);
+  Set _parseNull() => this.parser.parseNull();
+  Parser _derive(token) => this.parser.derive();
+}
